16bef66b600774696752036e7c69bdc076e43cdf
Lorenzo Pezzotti <lorenzo.pezzotti01@gmail.com>
2025-09-29 18:17:53 +0200

    Simplify DRrootify for 2025 usage
    In TB2025 DRrootify will be used only in combination with the blended
    ntupled creator. Here I simply its execution to use it in test mode on a
    simple test file and DREvent from 2025.

--- 2025_SPS/scripts/DRrootify.py
+++ 2025_SPS/scripts/DRrootify.py
@@ -9,8 +9,9 @@
 ## \start date: 16 August 2021
 ##
 ## Updated for TB2023 by Yuchen Cai and Iacopo Vivarelli 
+##
+## Updated for TB2025 by Lorenzo Pezzotti
 ##**************************************************
-
 
 import DREvent
 import ROOT 
@@ -26,36 +27,28 @@ class DRrootify:
         '''Class Constructor'''
         self.drf = None
         self.drffile = ROOT.TFile(fname + ".root","recreate")
-        self.tbtree = ROOT.TTree("CERNSPS2024","CERNSPS2024")
+        self.tbtree = ROOT.TTree("CERNSPS2025","CERNSPS2025")
         self.EventNumber = array('i',[0])
         self.EventSpill = array('i',[0])
-        self.Eventms = array('i',[0])
-        self.Eventsec = array('i',[0])
-        self.Eventmin = array('i',[0])
-        self.Eventhour = array('i',[0])
-        self.Eventday = array('i',[0])
+        #self.EventTime = array('l',[0])
         self.NumOfPhysEv = array('i',[0])
         self.NumOfPedeEv = array('i',[0])
         self.NumOfSpilEv = array('i',[0])
         self.TriggerMask = array('l',[0])
-        self.ADCs = array('i',[-1]*128)
+        self.ADCs = array('i',[-1]*224)
         self.TDCsval = array('i',[-1]*48)
         self.TDCscheck = array('i',[-1]*48)
 
         self.tbtree.Branch("EventNumber",self.EventNumber,'EventNumber/I')
         self.tbtree.Branch("EventSpill",self.EventSpill,'EventSpill/I')
-        self.tbtree.Branch("Eventms",self.Eventms,'Eventms/I')
-        self.tbtree.Branch("Eventsec",self.Eventsec,'Eventsec/I')
-        self.tbtree.Branch("Eventmin",self.Eventmin,'Eventmin/I')
-        self.tbtree.Branch("Eventhour",self.Eventhour,'Eventhour/I')
-        self.tbtree.Branch("Eventday",self.Eventday,'Eventday/I')
+        #self.tbtree.Branch("EventTime",self.EventTime,'Eventday/L')
         self.tbtree.Branch("NumOfPhysEv",self.NumOfPhysEv,'NumOfPhysEv/I')
         self.tbtree.Branch("NumOfPedeEv",self.NumOfPedeEv,'NumOfPedeEv/I')
         self.tbtree.Branch("NumOfSpilEv",self.NumOfSpilEv,'NumOfSpilEv/I')
         self.tbtree.Branch("TriggerMask",self.TriggerMask,'TriggerMask/L')
-        self.tbtree.Branch("ADCs",self.ADCs,'ADCs[128]/I')
-        self.tbtree.Branch("TDCsval",self.TDCsval,'TDCsval[48]/I')
-        self.tbtree.Branch("TDCscheck",self.TDCscheck,'TDCscheck[48]/I')
+        self.tbtree.Branch("ADCs",self.ADCs,'ADCs[224]/I')
+        #self.tbtree.Branch("TDCsval",self.TDCsval,'TDCsval[48]/I')
+        #self.tbtree.Branch("TDCscheck",self.TDCscheck,'TDCscheck[48]/I')
 
     def ReadandRoot(self):
         if self.drf == None:
@@ -68,16 +61,16 @@ class DRrootify:
             for ch in range(0,48):
                 self.TDCsval[ch] = -1
                 self.TDCscheck[ch] = -1
-            for ch in range(0,128):
+            for ch in range(0,224):
                 self.ADCs[ch] = -1
             evt = DREvent.DRdecode(line)
+            print("------------")
+            print(evt.ADCs)
+            print(evt.TDCs)
+            print("------------")
             self.EventNumber[0] = evt.EventNumber
             self.EventSpill[0] = evt.SpillNumber
-            self.Eventms[0] = int( (evt.EventTime.split("-"))[4] ) #take evt microseconds
-            self.Eventsec[0] = int( (evt.EventTime.split("-"))[3] ) #take evt seconds
-            self.Eventmin[0] = int( (evt.EventTime.split("-"))[2] ) #take evt minutes
-            self.Eventhour[0] = int( (evt.EventTime.split("-"))[1] ) #take evt hours
-            self.Eventday[0] = int( (evt.EventTime.split("-"))[0] ) #take evt day
+            #self.EventTime[0] = evt.EventTime
             self.NumOfPhysEv[0] = evt.NumOfPhysEv
             self.NumOfPedeEv[0] = evt.NumOfPedeEv
             self.NumOfSpilEv[0] = evt.NumOfSpilEv
@@ -86,9 +79,9 @@ class DRrootify:
 	    #	print evt.TriggerMask
             for ch,val in evt.ADCs.items() :
                 self.ADCs[ch] = val
-            for ch,vals in evt.TDCs.items():
-                self.TDCsval[ch] = vals[0]
-                self.TDCscheck[ch] = vals[1]
+            #for ch,vals in evt.TDCs.items():
+            #    self.TDCsval[ch] = vals[0]
+            #    self.TDCscheck[ch] = vals[1]
             self.tbtree.Fill()
 
         print( "--->End rootification ")
@@ -110,80 +103,29 @@ def main():
     parser.add_argument('--debug', action='store_true', dest='debug',
                         default=False,
                         help='Print more information')
-    parser.add_argument('-o','--output_dir', action='store', dest='ntuplepath',
-                        default='/eos/user/i/ideadr/TB2023_H8/rawNtuple',
-                        help='output root files will be stored in this directory')
-    parser.add_argument('-i','--input_dir', action='store', dest='datapath',
-                        default='/eos/user/i/ideadr/TB2023_H8/rawDataDreamDaq',
-                        help='output root files will be stored in this directory')
+    parser.add_argument('-o','--output_file', action='store', dest='outputfile',
+                        default='',
+                        help='output root file')
+    parser.add_argument('-i','--input_file', action='store', dest='inputfile',
+                        default='',
+                        help='input file')
     par = parser.parse_args()
-
-
-
-    #Get list of files on rawData and rawNtuple
-    #
-    datapath = par.datapath
-    ntuplepath = par.ntuplepath
-
-    print( 'Input directory ' + datapath )
-    print( 'Output directory ' + ntuplepath )
-
-    if not os.path.isdir(datapath):
-        print( 'ERROR! Input directory ' + datapath + ' does not exist.' )
-        return -1
-
-    if not os.path.isdir(ntuplepath):
-        print( 'ERROR! Output directory ' + ntuplepath + ' does not exist.' )
-        return -1
-
-    datafls = [x.split(".bz2")[0] for x in glob.glob(datapath+"/sps*.bz2")]
-    datafls = [os.path.basename(x) for x in datafls]
-
-    ntuplfls = [x.split(".root")[0]+".txt" for x in glob.glob(ntuplepath+"/*.root")]
-    ntuplfls = [os.path.basename(x) for x in ntuplfls]
-    newfls = list(set(datafls)-set(ntuplfls))
-
-
-
-    if par.debug: 
-        print ('\n This is the list of potential raw data files to be converted \n')
-        for x in datafls:
-            print( x)
-
-        print ('\n This is the list of data files already converted \n')
-        for x in ntuplfls:
-            print( x)
-                
-        print ('\n This is the list of data files that will be converted \n')
-        for x in newfls:
-            print( x)
-        quit()
-
-    #Rootify those data
+   
+    infile = par.inputfile
+    outfile = par.outputfile
     
-    print ("Hi!")
-    if newfls:
-        print( str(len(newfls))+" new files found")
+    print("Hi!")
+    print("Going to rootify "+infile+" in "+outfile)
 
-    for fl in newfls:
-        print( "->Found new file to be rootified: " + str(fl) )
-        os.system("bzip2 -d -k "+datapath+ '/' + str(fl)+".bz2")
-        print( "--->"+str(fl)+".bz2 decompressed")
-        fname = fl[0:-4] # remove .txt in the name
-        dr = DRrootify()
-        dr.Open(datapath+'/' +fname)
-        if not dr.ReadandRoot(fname):
-            print ("Problems in rootifying " + datapath + '/' + fname) 
-        dr.Write()
-        os.system("rm "+datapath+'/' + fl) # remove decompressed txt files
-        os.system("mv "+datapath+ '/' + str(fl[0:-4])+".root "+ntuplepath + '/') # move output ntuple to output dir
-    else:
-        print( "->No new files found"            )
+    dr=DRrootify(outfile)
+    dr.Open(infile)
+    if not dr.ReadandRoot():
+        print ("Problems in rootifying ") 
+    dr.Write()
 
     print( "Bye!")
 
 ##**************************************************
-
 
 if __name__ == "__main__":
     main()
